7c06a92184cfb1628954f9635c17ac49f90acb62
羅士釗 <robertlsj@hotmail.com>
2017-08-07 12:58:07 +0800

    Migrate Project

--- BalanceSheet/BalanceSheet/Controllers/HomeController.cs
+++ BalanceSheet/BalanceSheet/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BalanceSheet.Models;
 
 namespace BalanceSheet.Controllers
 {
@@ -10,7 +11,47 @@ namespace BalanceSheet.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            IList<BookingViewModels> bookingModels = default(IList<BookingViewModels>);
+            BookingViewModels accountRecord1 = new BookingViewModels(){
+                Kind = "收入",
+                Date = "2017/08/03",
+                Amount = 1200
+            };
+            bookingModels.Add(accountRecord1);
+
+            BookingViewModels accountRecord2 = new BookingViewModels()
+            {
+                Kind = "支出",
+                Date = "2017/08/03",
+                Amount = 500
+            };
+            bookingModels.Add(accountRecord2);
+
+            BookingViewModels accountRecord3 = new BookingViewModels()
+            {
+                Kind = "支出",
+                Date = "2017/08/04",
+                Amount = 700
+            };
+            bookingModels.Add(accountRecord3);
+
+            BookingViewModels accountRecord4 = new BookingViewModels()
+            {
+                Kind = "收入",
+                Date = "2017/08/05",
+                Amount = 800
+            };
+            bookingModels.Add(accountRecord4);
+
+            BookingViewModels accountRecord5 = new BookingViewModels()
+            {
+                Kind = "支出",
+                Date = "2017/08/06",
+                Amount = 2000
+            };
+            bookingModels.Add(accountRecord5);
+
+            return View(bookingModels);
         }
 
         public ActionResult About()
---
+++ BalanceSheet/BalanceSheet/Models/ViewModels/BookingViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BalanceSheet.Models
+{
+    public class BookingViewModels
+    {
+        public string Kind { get; set; }        
+        public string Date { get; set; }
+        public int Amount { get; set; }
+    }
+}
